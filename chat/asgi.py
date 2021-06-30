3b3e424bf0ad8c04d692b5da1962ef3d62f40db2
abhinav33agrawal <52850849+abhinav33agrawal@users.noreply.github.com>
2021-06-30 13:47:13 +0530

    Update asgi.py

--- chat/asgi.py
+++ chat/asgi.py
@@ -4,13 +4,14 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
 from django.core.asgi import get_asgi_application
 
 application = get_asgi_application()
 
-import os
 from channels.routing import ProtocolTypeRouter, URLRouter
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
 from channels.auth import AuthMiddlewareStack
 from core import routing as core_routing
