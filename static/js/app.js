90f494e04af12fe345fbdc2db2bcae298eb32b59
abhinav33agrawal <52850849+abhinav33agrawal@users.noreply.github.com>
2021-06-30 14:07:03 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
 
   //    let socket = new WebSocket(`ws://127.0.0.1:8000/?session_key=${sessionKey}`);
   var socket = new WebSocket(
-    "wss://" + window.location.host + "/ws?session_key=${sessionKey}"
+    "wss://" + window.location.host + "/wss?session_key=${sessionKey}"
   );
 
   chatInput.keypress(function (e) {
